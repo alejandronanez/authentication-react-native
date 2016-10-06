461a9eec2442b0f119f637886c9cf1e6350cf5d4
Alejandro Nanez <alejonanez@gmail.com>
2016-10-05 23:35:41 -0500

    Text inputs styling + component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './button';
 export * from './card';
 export * from './card_section';
 export * from './header';
+export * from './input';
--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common'; 
+import { Button, Card, CardSection, Input } from './common'; 
 
 class LoginForm extends Component {
 	state = { text: '' };
@@ -10,10 +9,10 @@ class LoginForm extends Component {
 		return (
 			<Card>
 				<CardSection>
-					<TextInput
+					<Input
+						label="Email"
 						value={this.state.text}
 						onChangeText={text => this.setState({ text })}
-						style={{ height: 20, width: 100 }}
 					/>
 				</CardSection>
 				<CardSection />
